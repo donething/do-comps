9a33001d446635f5484db2986044e441b1cfe82a
Donething <donething.net@gmail.com>
2022-10-28 16:32:17 +0800

    不打包source map

--- rollup.config.js
+++ rollup.config.js
@@ -14,12 +14,12 @@ export default [
       {
         file: packageJson.main,
         format: "cjs",
-        sourcemap: true,
+        // sourcemap: true,
       },
       {
         file: packageJson.module,
         format: "esm",
-        sourcemap: true,
+        // sourcemap: true,
       },
     ],
     plugins: [
